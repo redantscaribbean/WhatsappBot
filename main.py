dc31b442ee29290a73493f37b12198236c3adaf1
Jairaj Ramkissoon <jairaj@live.com>
2020-09-13 01:46:41 -0400

    Added 200 OK

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, validator, Required, fields
 import Models.Inbound as Inbound
 from http import HTTPStatus
+import bottle
 
 app = FastAPI()
 
@@ -12,4 +13,4 @@ db = []
 
 @app.post('/webhook/',  status_code=200)
 async def index(message: Inbound.Message):
-    return HTTPStatus.OK
+    return bottle.HTTPResponse(status=200)
