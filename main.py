3d00d27401d789d4a3b71bc10c0f52b386cde812
Jairaj Ramkissoon <jairaj@live.com>
2020-09-22 22:40:04 -0400

    Added Mongo

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, validator, Required, fields
 import Models.Inbound as Inbound
+import pymongo
+from pymongo import MongoClient
+import json
 
 app = FastAPI()
 
-db = []
+
+client = pymongo.MongoClient("mongodb+srv://redants:disruptiveCaribbean@whtatsappdev.e5gab.azure.mongodb.net/Whatsapp?retryWrites=true&w=majority")
+db = client["Whatsapp"]
+collection = db["Profile"]
 
 
 @app.post('/webhook/',  status_code=200)
 async def index(message: Inbound.Message):
+    collection.insert_one(message.dict())
     return message
